d6c20f250d65ee27e51348c3c640e06f6f3df637
asiFREAK <ra0182108@gmail.com>
2023-11-29 18:46:39 +0530

    varibles

--- 02variables/main.go
+++ 02variables/main.go
@@ -2,6 +2,46 @@ package main
 
 import "fmt"
 
+const Anothervarible string = "bdbx" // public
+
 func main() {
-	fmt.Println("Variables")
+	var username string = "Rishav"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallint uint8 = 255
+	fmt.Println(smallint)
+	fmt.Printf("Variable is of type: %T \n", smallint)
+
+	var smallfloat float64 = 255.327532437875397
+	fmt.Println(smallfloat)
+	fmt.Printf("Variable is of type: %T \n", smallfloat)
+
+	// default values
+
+	var newvariable int
+	fmt.Println(newvariable)
+	fmt.Printf("Variable is of type: %T \n", newvariable)
+
+	// implicit type
+
+	var hello = 253
+	fmt.Println(hello)
+	fmt.Printf("Variable is of type: %T \n", hello)
+
+	// no var
+
+	countuser := "30"
+	fmt.Println(countuser)
+	fmt.Printf("Variable is of type: %T \n", countuser)
+
+	// const var
+
+	fmt.Println(Anothervarible)
+	fmt.Printf("Variable is of type: %T \n", Anothervarible)
+
 }
